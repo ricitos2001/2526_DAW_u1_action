d4d6b1d66a7c83bb5e93b731c1ba8ec8968ddeb8
Eduardo Fernández Oliver <efernandez@iesrafaelalberti.es>
2025-09-29 19:01:39 +0200

    Fix greeting message in saludo function

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
 def saludo(nombre: str) -> str:
-    return f"Hol, {nombre}!" # Cambio para forzar error. Para que no falle: f"Hola, {nombre}!"
+    return f"Hola, {nombre}!" # Cambio para forzar error. Para que no falle: f"Hola, {nombre}!"

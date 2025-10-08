eac2e34c0db3b029e2bb015a900e708a2999445d
ricitos2001 <cesar2001ricitos@gmail.com>
2025-10-08 19:12:02 +0200

    prueba realizada con exito

--- update_readme.py
+++ update_readme.py
@@ -17,7 +17,7 @@ def update_readme(status: str):
     for line in lines:
         new_lines.append(line)
         if line.strip() == "## Estado de los tests":
-            new_lines.append(status + " - " + timestamp + "\n")
+            new_lines.append("- " + status + " - " + timestamp + "\n")
 
     with open("README.md", "w", encoding="utf-8") as f:
         f.writelines(new_lines)
